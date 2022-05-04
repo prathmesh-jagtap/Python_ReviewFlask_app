aeec94bb9fe90cad6844c2912ea82fb8b661be0c
prathmesh-jagtap <61873247+prathmesh-jagtap@users.noreply.github.com>
2022-05-04 12:23:52 +0530

    scr directory added

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,7 @@
 import logging
 
 
-logging.basicConfig(filename='logger.log',
+logging.basicConfig(filename='../logger.log',
                     filemode='w',
                     level=logging.DEBUG,
                     format="%(asctime)s : %(levelname)s  %(name)s - %(message)s",
